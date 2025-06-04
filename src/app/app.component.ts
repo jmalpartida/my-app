35e13feb93777df580bfc01110a8deddd3e30ff4
Juan Malpartida <68925176+jmalpartida@users.noreply.github.com>
2025-06-03 23:03:39 -0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,8 @@ import { RouterOutlet } from '@angular/router';
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
+  template: `<h1>Hello world!</h1>`,
 })
 export class AppComponent {
   title = 'my-app';
-  template: `<h1>Hello world!</h1>`,
 }
